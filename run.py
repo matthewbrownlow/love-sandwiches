a8b80ff8d11e827b2aa9de7e17a13d8a84793874
matthewbrownlow <>
2023-07-29 12:26:24 +0000

    Add user request loop to repeat request for data until data provided is valid

--- run.py
+++ run.py
@@ -13,18 +13,24 @@ GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('love_sandwiches')
 
 
-def get_sales_data():
+def get_sales_data(): # This function checks for errors. If there are no errors, it will return True, and the "data is valid" print statement should appear, and the while loop is stopped with the break keyword. If our validate_data function encounters an error, it will print the error message to the terminal and return False. So the while loop will repeat it's request for data until the data provided is valid
     """
     Get sales figures input from the user.
     """
-    print("Please enter sales data from th last market.")
-    print("Data should be six numbers, seperated by commas.")
-    print("Example: 10,20,30,40,50,60\n") # \n Creates a new line in the terminal when the code is exicuted
+    while True: # Asks for the users data that then converts the string of data from the user into a list of values 
+        print("Please enter sales data from th last market.")
+        print("Data should be six numbers, seperated by commas.")
+        print("Example: 10,20,30,40,50,60\n") # \n Creates a new line in the terminal when the code is exicuted
 
-    data_str = input("Enter your data here: ")
+        data_str = input("Enter your data here: ")
 
-    sales_data = data_str.split(",")
-    validate_data(sales_data)
+        sales_data = data_str.split(",")
+
+        if validate_data(sales_data): # Calls the validate data function, passing it the sales_data list
+            print("Data is valid!")
+            break
+    
+    return sales_data
 
 
 def validate_data(values):
@@ -40,8 +46,11 @@ def validate_data(values):
             raise ValueError(f"Exactly 6 values required, you provided {len(values)}")
     except ValueError as e:
         print(f"Invalid data: {e}, please try again.\n")
+        return False
+
+    return True
 
 
 
 
-get_sales_data()
+data = get_sales_data()
